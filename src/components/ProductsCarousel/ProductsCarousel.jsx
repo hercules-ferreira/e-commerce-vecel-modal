1e30688901fbb8dbb6ca143713f536649a0ffd7a
Hercules Ferreira <olcaminote82019@gmail.com>
2023-09-03 10:57:58 -0300

    carousel

--- src/components/ProductsCarousel/ProductsCarousel.jsx
+++ src/components/ProductsCarousel/ProductsCarousel.jsx
@@ -5,74 +5,58 @@ export const ProductsCarousel = () => {
     <section className={styles.containerProducts}>
       <div>
         <div className={styles.productsCarousel}>
-          <img
-            src="src/assets/carrousel/smartphone.png"
-            width={100}
-            height={100}
-            alt="Product 1"
-          />
-          <div>
-            <h2>Iphone 11 PRO MAX BRANCO</h2>
+          <img src="src\assets\carrousel\smartphone-bg.png" alt="Product 1" />
+          <div className={styles.margin}>
+            <h2 className={styles.title}> Iphone 11 PRO MAX BRANCO</h2>
             <p className={styles.priceOld}>R$ 15000</p>
-            <p className={styles.priceNew}>12000</p>
-            <p className={styles.details}>ou 12x de R$ 1000,00 sem juros</p>
+            <p className={styles.priceNew}>R$ 12000</p>
+            <p className={styles.description}>ou 12x de R$ 1000,00 sem juros</p>
+            <p className={styles.delivery}>Frete grátis</p>
             <div className={styles.btn_section}></div>
-            <button className={styles.btn}>Compre</button>
           </div>
+          <button className={styles.btn}>Compre</button>
         </div>
       </div>
       <div>
         <div className={styles.productsCarousel}>
-          <img
-            src="src/assets/carrousel/smartphone.png"
-            width={100}
-            height={100}
-            alt="Product 1"
-          />
-          <div>
-            <h2>Iphone 11 PRO MAX BRANCO</h2>
+          <img src="src\assets\carrousel\smartphone-bg.png" alt="Product 1" />
+          <div className={styles.margin}>
+            <h2 className={styles.title}> Iphone 11 PRO MAX BRANCO</h2>
             <p className={styles.priceOld}>R$ 15000</p>
-            <p className={styles.priceNew}>12000</p>
-            <p className={styles.details}>ou 12x de R$ 1000,00 sem juros</p>
+            <p className={styles.priceNew}>R$ 12000</p>
+            <p className={styles.description}>ou 12x de R$ 1000,00 sem juros</p>
+            <p className={styles.delivery}>Frete grátis</p>
             <div className={styles.btn_section}></div>
-            <button className={styles.btn}>Compre</button>
           </div>
+          <button className={styles.btn}>Compre</button>
         </div>
       </div>
       <div>
         <div className={styles.productsCarousel}>
-          <img
-            src="src/assets/carrousel/smartphone.png"
-            width={100}
-            height={100}
-            alt="Product 1"
-          />
-          <div>
-            <h2>Iphone 11 PRO MAX BRANCO</h2>
+          <img src="src\assets\carrousel\smartphone-bg.png" alt="Product 1" />
+          <div className={styles.margin}>
+            <h2 className={styles.title}> Iphone 11 PRO MAX BRANCO</h2>
             <p className={styles.priceOld}>R$ 15000</p>
-            <p className={styles.priceNew}>12000</p>
-            <p className={styles.details}>ou 12x de R$ 1000,00 sem juros</p>
+            <p className={styles.priceNew}>R$ 12000</p>
+            <p className={styles.description}>ou 12x de R$ 1000,00 sem juros</p>
+            <p className={styles.delivery}>Frete grátis</p>
             <div className={styles.btn_section}></div>
-            <button className={styles.btn}>Compre</button>
           </div>
+          <button className={styles.btn}>Compre</button>
         </div>
       </div>
       <div>
         <div className={styles.productsCarousel}>
-          <img
-            src="src\assets\carrousel\smartphone.png"
-            width={100}
-            height={100}
-            alt="Product 1"
-          />
-          <div>
-            <h2>Iphone 11 PRO MAX BRANCO</h2>
+          <img src="src\assets\carrousel\smartphone-bg.png" alt="Product 1" />
+          <div className={styles.margin}>
+            <h2 className={styles.title}> Iphone 11 PRO MAX BRANCO</h2>
             <p className={styles.priceOld}>R$ 15000</p>
-            <p className={styles.priceNew}>12000</p>
-            <p className={styles.details}>ou 12x de R$ 1000,00 sem juros</p>
+            <p className={styles.priceNew}>R$ 12000</p>
+            <p className={styles.description}>ou 12x de R$ 1000,00 sem juros</p>
+            <p className={styles.delivery}>Frete grátis</p>
             <div className={styles.btn_section}></div>
-            <button className={styles.btn}>Compre</button>
           </div>
+          <button className={styles.btn}>Compre</button>
         </div>
       </div>
     </section>
